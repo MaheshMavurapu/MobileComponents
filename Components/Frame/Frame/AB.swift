bb4580783a711d97803269ee24df46d9a39ee4ac
Mahesh Mavurapu <maheshmavurapu@Maheshs-MacBook-Pro.local>
2018-12-19 20:55:24 +0530

    Pod installation done material components

---
+++ Components/Frame/Frame/AB.swift
@@ -0,0 +1,9 @@
+//
+//  AB.swift
+//  Frame
+//
+//  Created by Mahesh Mavurapu on 19/12/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+import Foundation
---
+++ Components/Frame/Frame/Frame.h
@@ -0,0 +1,19 @@
+//
+//  Frame.h
+//  Frame
+//
+//  Created by Mahesh Mavurapu on 19/12/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Frame.
+FOUNDATION_EXPORT double FrameVersionNumber;
+
+//! Project version string for Frame.
+FOUNDATION_EXPORT const unsigned char FrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Frame/PublicHeader.h>
+
+
